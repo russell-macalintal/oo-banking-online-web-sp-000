6975a5394de3731d3640ce63776f63651b0f3155
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-27 05:52:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,10 @@
 class BankAccount
-  attr_accessor :name, :balance, :status
+  attr_accessor :balance, :status
+  attr_reader :name
+  
   def initialize(name)
     @name = name
     @balance = 1000
-    @status = open
+    @status = "open"
   end
 end
